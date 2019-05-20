260fdf2507654d9e83b327d6b4e42e91adb7f2f8
Jose Annunziato <jannunzi@gmail.com>
2019-05-20 15:12:00 -0400

    latest

--- src/components/CourseEditor.js
+++ src/components/CourseEditor.js
@@ -5,13 +5,25 @@ import TopicPills from "./TopicPills";
 
 export default class CourseEditor
     extends React.Component {
+    constructor(props) {
+        super(props)
+        const pathname = window.location.pathname
+        const paths = pathname.split('/')
+        const courseId = paths[2]
+        console.log(courseId)
+        this.courses = props.courses
+        this.state = {
+            courseId: courseId,
+            course: this.courses.find(course => course.id == courseId)
+        }
+    }
     render() {
         return(
             <div>
-                <h2>Course Editor</h2>
+                <h2>{this.state.course.title} {this.state.course.id}</h2>
                 <div className="row">
                     <div className="col-4 left">
-                        <ModuleList/>
+                        <ModuleList modules={this.state.course.modules}/>
                     </div>
                     <div className="col-8 right">
                         <LessonTabs/>
